9f9428590b0929d2e73c79b6a93a1f80ea0a644a
Alexis Tuchinda <50222834+AlexisTuchinda@users.noreply.github.com>
2021-03-04 18:45:44 -0800

    Installed Node

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import * as content from "./content";
+import Home from "./Home/Home";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
---
+++ src/content.js
@@ -0,0 +1,44 @@
+//export static images/resources
+import muralLogo from "./res/Mural_Logo.png";
+import alienInvaders from "./res/Alien_Invaders_Logo.png";
+import computerAI from "./res/Computer_AI_reps.png";
+
+export const Logo =
+  "https://piskel-imgstore-b.appspot.com/img/5e9f2178-7873-11eb-9c8d-f11e32412b5c.gif";
+
+export const GitHub = "https://github.com/AlexisTuchinda";
+export const GitHub_Logo =
+  "https://image.flaticon.com/icons/png/512/25/25231.png";
+
+export const Trinket_Logo =
+  "https://ee5817f8e2e9a2e34042-3365e7f0719651e5b8d0979bce83c558.ssl.cf5.rackcdn.com/python.png";
+export const Trinket = "https://trinket.io/library/trinkets";
+
+export const Projects = [
+  {
+    link: "https://trinket.io/python/bea3037ff1",
+    title: "Computer Matching AI",
+    description:
+      "An algorithm that predicts the user's next input through pattern analysis.",
+    details: "(Programmed 2021)",
+    image: computerAI
+  },
+  {
+    link: "https://github.com/AlexisTuchinda/ShareSpace",
+    title: "Mural",
+    description: "A social media platform for multi-media art.",
+    details: "[Unreleased] (Programmed and Uploaded to GitHub 2020)",
+    image: muralLogo
+  },
+  {
+    link: "https://github.com/AlexisTuchinda/Alien-Invaders---Basic",
+    title: "Alien Invaders",
+    description:
+      "A low-graphic replication of the classic arcade game Alien Invaders.",
+    details: "(Uploaded to GitHub 2019)",
+    image: alienInvaders
+  }
+];
+
+export const Bio =
+  "I am a computer programmer who is interested in dynamic programming and algorithms. I have the most experience with Python but have worked with other programming languages and platforms such as React.js.";
